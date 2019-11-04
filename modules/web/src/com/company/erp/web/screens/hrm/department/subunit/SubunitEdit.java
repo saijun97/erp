c090662a2535e020297052a074f4529f30e710ab
srj <saiyaljuttun@hotmail.com>
2019-11-04 15:42:12 +0400

    modified messagePack path to refactored path

--- modules/web/src/com/company/erp/web/screens/hrm/department/subunit/SubunitEdit.java
+++ modules/web/src/com/company/erp/web/screens/hrm/department/subunit/SubunitEdit.java
@@ -1,7 +1,7 @@
 package com.company.erp.web.screens.hrm.department.subunit;
 
-import com.haulmont.cuba.gui.screen.*;
 import com.company.erp.entity.hrm.dept.Subunit;
+import com.haulmont.cuba.gui.screen.*;
 
 @UiController("erp_Subunit.edit")
 @UiDescriptor("subunit-edit.xml")
