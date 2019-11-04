832b27940c0838bec3a97bb484fdfdd348b74eb4
srj <saiyaljuttun@hotmail.com>
2019-11-04 13:43:05 +0400

    added responsive side menu

---
+++ modules/web/src/com/company/erp/web/screens/general/respsidemenu/ExtMainScreen.java
@@ -0,0 +1,11 @@
+package com.company.erp.web.screens.general.respsidemenu;
+
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+import com.haulmont.cuba.web.app.main.MainScreen;
+
+
+@UiController("extMainScreen")
+@UiDescriptor("ext-main-screen.xml")
+public class ExtMainScreen extends MainScreen {
+}
