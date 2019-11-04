8e004fa1fb1fc4e1885ff52a40a90a31efdd1309
srj <saiyaljuttun@hotmail.com>
2019-11-04 16:03:19 +0400

    added editor and browser screens for employee

---
+++ modules/web/src/com/company/erp/web/screens/hrm/employee/EmployeeEdit.java
@@ -0,0 +1,11 @@
+package com.company.erp.web.screens.hrm.employee;
+
+import com.haulmont.cuba.gui.screen.*;
+import com.company.erp.entity.hrm.Employee;
+
+@UiController("erp_Employee.edit")
+@UiDescriptor("employee-edit.xml")
+@EditedEntityContainer("employeeDc")
+@LoadDataBeforeShow
+public class EmployeeEdit extends StandardEditor<Employee> {
+}
