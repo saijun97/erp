23d18468718931f1786044d1071ced186da1135b
srj <saiyaljuttun@hotmail.com>
2019-11-04 15:05:16 +0400

    added constraints and more attributes

--- modules/global/src/com/company/erp/entity/hrm/Employee.java
+++ modules/global/src/com/company/erp/entity/hrm/Employee.java
@@ -1,13 +1,20 @@
 package com.company.erp.entity.hrm;
 
+import com.company.erp.entity.general.GenderSelect;
+import com.company.erp.entity.hrm.dept.Department;
+import com.company.erp.entity.hrm.dept.Subunit;
 import com.haulmont.chile.core.annotations.NamePattern;
 import com.haulmont.cuba.core.entity.StandardEntity;
 import com.haulmont.cuba.core.entity.annotation.CaseConversion;
+import com.haulmont.cuba.core.entity.annotation.Lookup;
+import com.haulmont.cuba.core.entity.annotation.LookupType;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import java.util.Date;
+import java.util.List;
 
 @NamePattern("%s %s %s|empId,firstName,lastName")
 @Table(name = "ERP_EMPLOYEE")
@@ -34,11 +41,82 @@ public class Employee extends StandardEntity {
     @Column(name = "LAST_NAME", nullable = false)
     protected String lastName;
 
+    @NotNull
+    @Column(name = "GENDER", nullable = false)
+    protected Integer gender;
+
     @Past(message = "Date of birth should be in the past.")
     @Temporal(TemporalType.DATE)
     @NotNull
     @Column(name = "DOB", nullable = false)
     protected Date dob;
+
+    @Lookup(type = LookupType.DROPDOWN, actions = {"lookup", "open", "clear"})
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "DEPARTMENT_ID")
+    protected Department department;
+
+    @OneToMany(mappedBy = "employee")
+    protected List<Subunit> subunit;
+
+    @Column(name = "MOBILE_PHONE", unique = true)
+    protected String mobilePhone;
+
+    @Column(name = "HOME_PHONE")
+    protected String homePhone;
+
+    @Email(message = "Not an email!")
+    @Column(name = "EMAIL", unique = true)
+    protected String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public List<Subunit> getSubunit() {
+        return subunit;
+    }
+
+    public void setSubunit(List<Subunit> subunit) {
+        this.subunit = subunit;
+    }
+
+    public GenderSelect getGender() {
+        return gender == null ? null : GenderSelect.fromId(gender);
+    }
+
+    public void setGender(GenderSelect gender) {
+        this.gender = gender == null ? null : gender.getId();
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 
     public String getNic() {
         return nic;
