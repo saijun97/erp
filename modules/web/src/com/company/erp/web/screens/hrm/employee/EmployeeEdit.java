ab4ec76affe0f3333ed0153f0659d6c001faa41b
srj <saiyaljuttun@hotmail.com>
2019-11-05 15:26:27 -0800

    refactored subunit attribute to lookup field and added dependency to department for subunit optionslist

--- modules/web/src/com/company/erp/web/screens/hrm/employee/EmployeeEdit.java
+++ modules/web/src/com/company/erp/web/screens/hrm/employee/EmployeeEdit.java
@@ -1,8 +1,16 @@
 package com.company.erp.web.screens.hrm.employee;
 
+import com.company.erp.entity.hrm.dept.Department;
+import com.company.erp.entity.hrm.dept.Subunit;
 import com.haulmont.cuba.gui.components.Action;
+import com.haulmont.cuba.gui.components.HasValue;
+import com.haulmont.cuba.gui.components.LookupField;
+import com.haulmont.cuba.gui.components.PickerField;
+import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.erp.entity.hrm.Employee;
+
+import javax.inject.Inject;
 
 @UiController("erp_Employee.edit")
 @UiDescriptor("employee-edit.xml")
