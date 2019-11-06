6c43afad8b3dd28ea685352ee83b284ea1593133
srj <saiyaljuttun@hotmail.com>
2019-11-05 16:00:24 -0800

    database scripts updated for postgres use

---
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -0,0 +1,60 @@
+-- begin ERP_DEPARTMENT
+create table ERP_DEPARTMENT (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    DEPARTMENT varchar(255) not null,
+    --
+    primary key (ID)
+)^
+-- end ERP_DEPARTMENT
+-- begin ERP_EMPLOYEE
+create table ERP_EMPLOYEE (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    EMP_ID varchar(255) not null,
+    NIC varchar(255),
+    FIRST_NAME varchar(255) not null,
+    MIDDLE_NAME varchar(255),
+    LAST_NAME varchar(255) not null,
+    GENDER integer not null,
+    DOB date not null,
+    DEPARTMENT_ID uuid not null,
+    SUBUNIT_ID uuid,
+    MOBILE_PHONE varchar(255),
+    HOME_PHONE varchar(255),
+    EMAIL varchar(255),
+    --
+    primary key (ID)
+)^
+-- end ERP_EMPLOYEE
+-- begin ERP_SUBUNIT
+create table ERP_SUBUNIT (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    SUBUNIT varchar(255) not null,
+    DEPARTMENT_ID uuid not null,
+    --
+    primary key (ID)
+)^
+-- end ERP_SUBUNIT
---
+++ modules/core/db/update/postgres/19/191105-1-createSubunit.sql
@@ -0,0 +1,15 @@
+create table ERP_SUBUNIT (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    SUBUNIT varchar(255) not null,
+    DEPARTMENT_ID uuid not null,
+    --
+    primary key (ID)
+);
