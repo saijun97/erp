2aca5c725e2831647c1da50385f7afa469e4942e
SRJ-Acer <saiyaljuttun@hotmail.com>
2019-11-07 19:45:41 +0400

    database scripts consolidated

--- modules/core/db/update/postgres/19/191105-0-dropCountry.sql
+++
@@ -1,2 +0,0 @@
-alter table erp_country rename to ERP_COUNTRY__U83779 ;
-alter table erp_employee drop constraint FK_ERP_EMPLOYEE_ON_COUNTRY ;
--- modules/core/db/update/postgres/19/191105-2-updateEmployee02.sql
+++
@@ -1,1 +0,0 @@
-create unique index IDX_ERP_EMPLOYEE_UK_EMP_ID on ERP_EMPLOYEE (EMP_ID) where DELETE_TS is null ;
