2a86a2a0dea03e52f3bb2dbcc78ca40e89f9081c
srj <saiyaljuttun@hotmail.com>
2019-12-10 09:43:20 +0400

    changed main datastore to postgreSQL

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -1,3 +1,22 @@
+-- begin ERP_EMPLOYEE_SKILL
+create table ERP_EMPLOYEE_SKILL (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    EMPLOYEE_ID uuid not null,
+    SKILL_ID uuid not null,
+    YEARS_OF_EXPERIENCE integer,
+    COMMENT varchar(255),
+    --
+    primary key (ID)
+)^
+-- end ERP_EMPLOYEE_SKILL
 -- begin ERP_DEPARTMENT
 create table ERP_DEPARTMENT (
     ID uuid,
@@ -14,33 +33,6 @@ create table ERP_DEPARTMENT (
     primary key (ID)
 )^
 -- end ERP_DEPARTMENT
--- begin ERP_EMPLOYEE
-create table ERP_EMPLOYEE (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    EMP_ID varchar(255) not null,
-    NIC varchar(255),
-    FIRST_NAME varchar(255) not null,
-    MIDDLE_NAME varchar(255),
-    LAST_NAME varchar(255) not null,
-    GENDER integer not null,
-    DOB date not null,
-    DEPARTMENT_ID uuid not null,
-    SUBUNIT_ID uuid,
-    MOBILE_PHONE varchar(255),
-    HOME_PHONE varchar(255),
-    EMAIL varchar(255),
-    --
-    primary key (ID)
-)^
--- end ERP_EMPLOYEE
 -- begin ERP_SUBUNIT
 create table ERP_SUBUNIT (
     ID uuid,
@@ -58,3 +50,47 @@ create table ERP_SUBUNIT (
     primary key (ID)
 )^
 -- end ERP_SUBUNIT
+-- begin ERP_EMPLOYEE
+create table ERP_EMPLOYEE (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    FIRST_NAME varchar(255) not null,
+    MIDDLE_NAME varchar(255),
+    LAST_NAME varchar(255) not null,
+    --
+    EMP_ID varchar(255) not null,
+    NIC varchar(255),
+    GENDER integer not null,
+    DOB date not null,
+    DEPARTMENT_ID uuid not null,
+    SUBUNIT_ID uuid,
+    MOBILE_PHONE varchar(255),
+    HOME_PHONE varchar(255),
+    EMAIL varchar(255),
+    --
+    primary key (ID)
+)^
+-- end ERP_EMPLOYEE
+-- begin ERP_SKILL
+create table ERP_SKILL (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    NAME varchar(255) not null,
+    --
+    DESCRIPTION varchar(255),
+    --
+    primary key (ID)
+)^
+-- end ERP_SKILL
