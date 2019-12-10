d85b1252e4611fd56ef8dbe871e9baec5cb9e1f0
srj <saiyaljuttun@hotmail.com>
2019-12-10 09:46:25 +0400

    removed mutator & accessor methods for Person superclass attributes

--- modules/global/src/com/company/erp/entity/hrm/employee/Employee.java
+++ modules/global/src/com/company/erp/entity/hrm/employee/Employee.java
@@ -146,30 +146,6 @@ public class Employee extends Person {
         this.dob = dob;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getEmpId() {
         return empId;
     }
