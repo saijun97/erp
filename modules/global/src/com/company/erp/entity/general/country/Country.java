913edc9fb79eadef9f2d0a9b38365ca1daba485b
srj <saiyaljuttun@hotmail.com>
2019-12-12 09:27:06 +0400

    added country and city submodules

--- modules/global/src/com/company/erp/entity/general/country/Country.java
+++ modules/global/src/com/company/erp/entity/general/country/Country.java
@@ -1,23 +1,22 @@
 package com.company.erp.entity.general.country;
 
 import com.haulmont.chile.core.annotations.Composition;
+import com.haulmont.chile.core.annotations.NamePattern;
 import com.haulmont.cuba.core.entity.*;
 import com.haulmont.cuba.core.entity.annotation.OnDelete;
 import com.haulmont.cuba.core.global.DeletePolicy;
 
 import javax.persistence.*;
 import javax.persistence.Entity;
-import java.util.Date;
 import java.util.List;
 
-
+@NamePattern("%s|country")
 @Table(name = "ERP_COUNTRY")
 @Entity(name = "erp_Country")
-public class Country extends BaseGenericIdEntity implements Creatable,Updatable,Versioned{
+public class Country extends StandardEntity{
     private static final long serialVersionUID = 6557717258140789980L;
 
-    @Id
-    @Column(name = "COUNTRY_CODE", nullable = false, unique = true, length = 10)
+    @Column(name = "COUNTRY_CODE", nullable = false, unique = true, length = 5)
     protected String countryCode;
 
     @Column(name = "COUNTRY", nullable = false, unique = true)
@@ -27,66 +26,6 @@ public class Country extends BaseGenericIdEntity implements Creatable,Updatable,
     @OnDelete(DeletePolicy.CASCADE)
     @OneToMany(mappedBy = "country")
     protected List<City> city;
-
-    @Override
-    public void setId(Object id) {
-
-    }
-
-    @Override
-    public Object getId() {
-        return null;
-    }
-
-    @Override
-    public Date getCreateTs() {
-        return null;
-    }
-
-    @Override
-    public void setCreateTs(Date date) {
-
-    }
-
-    @Override
-    public String getCreatedBy() {
-        return null;
-    }
-
-    @Override
-    public void setCreatedBy(String createdBy) {
-
-    }
-
-    @Override
-    public Date getUpdateTs() {
-        return null;
-    }
-
-    @Override
-    public void setUpdateTs(Date updateTs) {
-
-    }
-
-    @Override
-    public String getUpdatedBy() {
-        return null;
-    }
-
-    @Override
-    public void setUpdatedBy(String updatedBy) {
-
-    }
-
-    @Override
-    public Integer getVersion() {
-        return null;
-    }
-
-    @Override
-    public void setVersion(Integer version) {
-
-    }
 
     public void setCity(List<City> city) {
         this.city = city;
