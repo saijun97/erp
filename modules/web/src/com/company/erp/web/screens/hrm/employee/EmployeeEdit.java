baad78c64c66e14b04b18c35aaa6c4d8733bcd5d
srj <saiyaljuttun@hotmail.com>
2019-12-18 13:57:14 +0400

    added fullName attribute and added function to compute fullName from firstName,middleName and lastName attributes

--- modules/web/src/com/company/erp/web/screens/hrm/employee/EmployeeEdit.java
+++ modules/web/src/com/company/erp/web/screens/hrm/employee/EmployeeEdit.java
@@ -1,12 +1,54 @@
 package com.company.erp.web.screens.hrm.employee;
 
+import com.haulmont.cuba.gui.components.TextField;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.erp.entity.hrm.employee.Employee;
+
+import javax.inject.Inject;
 
 @UiController("erp_Employee.edit")
 @UiDescriptor("employee-edit.xml")
 @EditedEntityContainer("employeeDc")
 @LoadDataBeforeShow
 public class EmployeeEdit extends StandardEditor<Employee> {
+
+    @Inject
+    protected TextField<String> firstNameField;
+
+    @Inject
+    protected TextField<String> middleNameField;
+
+    @Inject
+    protected TextField<String> lastNameField;
+
+    @Inject
+    protected TextField<String> fullNameField;
+
+    String fullNameValue;
+
+    @Subscribe
+    protected void onBeforeCommitChanges(BeforeCommitChangesEvent event) {
+
+        if (firstNameField.getValue() != null) {
+
+            fullNameValue = firstNameField.getValue();
+        }
+
+        if (middleNameField.getValue() != null) {
+
+            fullNameValue += " " + middleNameField.getValue();
+
+        }
+
+        if (lastNameField.getValue() != null) {
+
+            fullNameValue += " " + lastNameField.getValue();
+
+        }
+
+        fullNameField.setValue(fullNameValue);
+
+    }
+
 
 }
