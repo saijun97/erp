c52561ddb80e0b5125c8fcf6ff8a3a8d45e2dafb
srj <saiyaljuttun@hotmail.com>
2019-12-20 16:19:23 +0400

    added country field to cityEdit screen

--- modules/web/src/com/company/erp/web/screens/general/country/city/CityEdit.java
+++ modules/web/src/com/company/erp/web/screens/general/country/city/CityEdit.java
@@ -1,11 +1,16 @@
 package com.company.erp.web.screens.general.country.city;
 
+import com.company.erp.entity.general.country.Country;
+import com.haulmont.cuba.gui.components.LookupPickerField;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.erp.entity.general.country.City;
+
+import javax.inject.Inject;
 
 @UiController("erp_City.edit")
 @UiDescriptor("city-edit.xml")
 @EditedEntityContainer("cityDc")
 @LoadDataBeforeShow
 public class CityEdit extends StandardEditor<City> {
+
 }
