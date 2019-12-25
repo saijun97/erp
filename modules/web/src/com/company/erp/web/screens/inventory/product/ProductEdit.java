7badf229d334715e88c76c35e99496e87652bdd2
SRJ-Acer <saiyaljuttun@hotmail.com>
2019-12-25 19:18:59 +0400

    created product subclass entity

---
+++ modules/web/src/com/company/erp/web/screens/inventory/product/ProductEdit.java
@@ -0,0 +1,11 @@
+package com.company.erp.web.screens.inventory.product;
+
+import com.haulmont.cuba.gui.screen.*;
+import com.company.erp.entity.inventory.Product;
+
+@UiController("erp_Product.edit")
+@UiDescriptor("product-edit.xml")
+@EditedEntityContainer("productDc")
+@LoadDataBeforeShow
+public class ProductEdit extends StandardEditor<Product> {
+}
