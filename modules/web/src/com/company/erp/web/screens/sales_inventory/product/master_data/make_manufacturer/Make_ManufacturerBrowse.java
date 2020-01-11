43ca09c9e87b809330348ce7e3792acd4de71e5e
saiya <saiyaljuttun@hotmail.com>
2020-01-12 03:44:02 +0400

    added screens for product, product category & make/manufacturer entities

---
+++ modules/web/src/com/company/erp/web/screens/sales_inventory/product/master_data/make_manufacturer/Make_ManufacturerBrowse.java
@@ -0,0 +1,11 @@
+package com.company.erp.web.screens.sales_inventory.product.master_data.make_manufacturer;
+
+import com.haulmont.cuba.gui.screen.*;
+import com.company.erp.entity.sales_inventory.product.master_data.make_manufacturer.Make_Manufacturer;
+
+@UiController("erp_Make_Manufacturer.browse")
+@UiDescriptor("make_manufacturer-browse.xml")
+@LookupComponent("make_ManufacturersTable")
+@LoadDataBeforeShow
+public class Make_ManufacturerBrowse extends StandardLookup<Make_Manufacturer> {
+}
