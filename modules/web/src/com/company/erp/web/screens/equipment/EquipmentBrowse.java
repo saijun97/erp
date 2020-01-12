57c8c22bb56dcd0555cfea6612bf51f99672b6b6
saiya <saiyaljuttun@hotmail.com>
2020-01-12 04:29:48 +0400

    created service requests(srf) and equipment screens

---
+++ modules/web/src/com/company/erp/web/screens/equipment/EquipmentBrowse.java
@@ -0,0 +1,11 @@
+package com.company.erp.web.screens.equipment;
+
+import com.haulmont.cuba.gui.screen.*;
+import com.company.erp.entity.crm.srf.Equipment;
+
+@UiController("erp_Equipment.browse")
+@UiDescriptor("equipment-browse.xml")
+@LookupComponent("equipmentsTable")
+@LoadDataBeforeShow
+public class EquipmentBrowse extends StandardLookup<Equipment> {
+}
