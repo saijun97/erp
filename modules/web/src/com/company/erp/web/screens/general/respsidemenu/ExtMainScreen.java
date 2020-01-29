3f59de3e58fd77fd642e53b1a3f2e7a31ebe84e5
saiya <saiyaljuttun@hotmail.com>
2020-01-29 07:08:40 +0400

    added chart widget for customer with total earnings

--- modules/web/src/com/company/erp/web/screens/general/respsidemenu/ExtMainScreen.java
+++ modules/web/src/com/company/erp/web/screens/general/respsidemenu/ExtMainScreen.java
@@ -1,5 +1,6 @@
 package com.company.erp.web.screens.general.respsidemenu;
 
+import com.haulmont.cuba.gui.screen.LoadDataBeforeShow;
 import com.haulmont.cuba.gui.screen.UiController;
 import com.haulmont.cuba.gui.screen.UiDescriptor;
 import com.haulmont.cuba.web.app.main.MainScreen;
@@ -7,5 +8,6 @@ import com.haulmont.cuba.web.app.main.MainScreen;
 
 @UiController("extMainScreen")
 @UiDescriptor("ext-main-screen.xml")
+@LoadDataBeforeShow
 public class ExtMainScreen extends MainScreen {
 }
