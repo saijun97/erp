a07df8625fa8601bb8bb82ff78f01b30144a9ca2
saiya <saiyaljuttun@hotmail.com>
2020-01-29 07:09:30 +0400

    created main-dashboard screen

---
+++ modules/web/src/com/company/erp/web/screens/general/dashboards/MainDashboard.java
@@ -0,0 +1,9 @@
+package com.company.erp.web.screens.general.dashboards;
+
+import com.haulmont.cuba.gui.screen.*;
+
+@UiController("erp_MainDashboard")
+@UiDescriptor("main-dashboard.xml")
+@LoadDataBeforeShow
+public class MainDashboard extends Screen{
+}
