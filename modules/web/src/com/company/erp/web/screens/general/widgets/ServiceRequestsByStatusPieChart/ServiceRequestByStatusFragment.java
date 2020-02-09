be5e62f11e5088393f814a6cdc95e4ae9d3d9b1f
saiya <saiyaljuttun@hotmail.com>
2020-02-09 10:04:20 +0400

    added service requests by status pie chart fragment

---
+++ modules/web/src/com/company/erp/web/screens/general/widgets/ServiceRequestsByStatusPieChart/ServiceRequestByStatusFragment.java
@@ -0,0 +1,22 @@
+package com.company.erp.web.screens.general.widgets.ServiceRequestsByStatusPieChart;
+
+import com.haulmont.cuba.gui.model.KeyValueCollectionLoader;
+import com.haulmont.cuba.gui.screen.*;
+
+import javax.inject.Inject;
+
+@UiController("erp_ServiceRequestByStatusFragment")
+@UiDescriptor("service-request-by-status-fragment.xml")
+public class ServiceRequestByStatusFragment extends ScreenFragment {
+    @Inject
+    protected KeyValueCollectionLoader salesPieChartLoader;
+
+    @Subscribe(target = Target.PARENT_CONTROLLER)
+    protected void onBeforeShow(Screen.BeforeShowEvent event) {
+
+        salesPieChartLoader.load();
+
+    }
+
+
+}
