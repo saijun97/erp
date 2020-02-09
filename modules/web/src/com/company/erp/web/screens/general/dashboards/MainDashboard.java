1bc38b2cde6d47056f67f2f603cf493053a5147a
saiya <saiyaljuttun@hotmail.com>
2020-02-09 10:02:59 +0400

    removed main dashboard screen

--- modules/web/src/com/company/erp/web/screens/general/dashboards/MainDashboard.java
+++
@@ -1,9 +0,0 @@
-package com.company.erp.web.screens.general.dashboards;
-
-import com.haulmont.cuba.gui.screen.*;
-
-@UiController("erp_MainDashboard")
-@UiDescriptor("main-dashboard.xml")
-@LoadDataBeforeShow
-public class MainDashboard extends Screen{
-}
