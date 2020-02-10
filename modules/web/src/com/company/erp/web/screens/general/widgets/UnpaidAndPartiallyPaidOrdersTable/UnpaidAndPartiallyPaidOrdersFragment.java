80e1fceb1d968e10c1557023ec1caad04da7f4a4
saiya <saiyaljuttun@hotmail.com>
2020-02-10 04:40:02 +0400

    added unpaid-and-partially-paid-orders fragment

--- modules/web/src/com/company/erp/web/screens/general/widgets/UnpaidAndPartiallyPaidOrdersTable/UnpaidAndPartiallyPaidOrdersFragment.java
+++ modules/web/src/com/company/erp/web/screens/general/widgets/UnpaidAndPartiallyPaidOrdersTable/UnpaidAndPartiallyPaidOrdersFragment.java
@@ -1,10 +1,23 @@
 package com.company.erp.web.screens.general.widgets.UnpaidAndPartiallyPaidOrdersTable;
 
-import com.haulmont.cuba.gui.screen.ScreenFragment;
-import com.haulmont.cuba.gui.screen.UiController;
-import com.haulmont.cuba.gui.screen.UiDescriptor;
+import com.company.erp.entity.sales_inventory.order.Order;
+import com.haulmont.cuba.gui.model.CollectionLoader;
+import com.haulmont.cuba.gui.screen.*;
+
+import javax.inject.Inject;
 
 @UiController("erp_UnpaidAndPartiallyPaidOrdersFragment")
 @UiDescriptor("unpaid-and-partially-paid-orders-fragment.xml")
 public class UnpaidAndPartiallyPaidOrdersFragment extends ScreenFragment {
+    @Inject
+    protected CollectionLoader<Order> ordersDl;
+
+    @Subscribe(target = Target.PARENT_CONTROLLER)
+    protected void onBeforeShow(Screen.BeforeShowEvent event) {
+
+        ordersDl.load();
+
+    }
+
+
 }
