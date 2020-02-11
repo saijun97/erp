8bf25f938af6063d2f0e0b0a82ac3c3f4f55e695
saiya <saiyaljuttun@hotmail.com>
2020-02-11 22:42:01 +0400

    added business logic to calculate total stock level based on individual store stock levels

--- modules/web/src/com/company/erp/web/screens/sales_inventory/product/ProductEdit.java
+++ modules/web/src/com/company/erp/web/screens/sales_inventory/product/ProductEdit.java
@@ -3,6 +3,7 @@ package com.company.erp.web.screens.sales_inventory.product;
 import com.haulmont.cuba.gui.components.CheckBox;
 import com.haulmont.cuba.gui.components.CurrencyField;
 import com.haulmont.cuba.gui.components.HasValue;
+import com.haulmont.cuba.gui.components.TextField;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.erp.entity.sales_inventory.product.Product;
 
@@ -21,15 +22,7 @@ public class ProductEdit extends StandardEditor<Product> {
     @Inject
     private CurrencyField<BigDecimal> vatPriceField;
 
-    @Subscribe("priceField")
-    public void onPriceFieldValueChange(HasValue.ValueChangeEvent<BigDecimal> event) {
-
-        vatPriceField.setValue(priceField.getValue());
-
-    }
-
-    @Subscribe("applyVatField")
-    public void onApplyVatFieldValueChange(HasValue.ValueChangeEvent<Boolean> event) {
+    public void calcVatPrice() {
 
         BigDecimal vatValue;
 
@@ -40,9 +33,47 @@ public class ProductEdit extends StandardEditor<Product> {
         }
 
         else vatPriceField.setValue(priceField.getValue());
+    }
 
+    public void calcQtyTotal (){
+
+        Integer qtyTotalValue = getEditedEntity().getQtyStore1() + getEditedEntity().getQtyStore2() + getEditedEntity().getQtyStore3();
+        getEditedEntity().setQtyTotal(qtyTotalValue);
 
     }
 
+    @Subscribe("priceField")
+    public void onPriceFieldValueChange(HasValue.ValueChangeEvent<BigDecimal> event) {
+
+        calcVatPrice();
+    }
+
+    @Subscribe("applyVatField")
+    public void onApplyVatFieldValueChange(HasValue.ValueChangeEvent<Boolean> event) {
+
+        calcVatPrice();
+
+    }
+
+    @Subscribe("qtyStore1Field")
+    protected void onQtyStore1FieldValueChange(HasValue.ValueChangeEvent<Integer> event) {
+
+        calcQtyTotal();
+
+    }
+
+    @Subscribe("qtyStore2Field")
+    protected void onQtyStore2FieldValueChange(HasValue.ValueChangeEvent<Integer> event) {
+
+        calcQtyTotal();
+
+    }
+
+    @Subscribe("qtyStore3Field")
+    protected void onQtyStore3FieldValueChange(HasValue.ValueChangeEvent<Integer> event) {
+
+        calcQtyTotal();
+
+    }
 
 }
