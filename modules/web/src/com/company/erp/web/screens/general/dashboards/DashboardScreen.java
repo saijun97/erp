48cf77520f15747727e8b04ada9308f2f7ed60b2
saiya <saiyaljuttun@hotmail.com>
2020-02-12 08:37:35 +0400

    added dashboard screen

---
+++ modules/web/src/com/company/erp/web/screens/general/dashboards/DashboardScreen.java
@@ -0,0 +1,16 @@
+package com.company.erp.web.screens.general.dashboards;
+
+import com.haulmont.cuba.gui.components.Window;
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+import javax.inject.Inject;
+
+@UiController("erp_DashboardScreen")
+@UiDescriptor("dashboard-screen.xml")
+public class DashboardScreen extends Screen {
+
+
+
+}
--- modules/web/src/com/company/erp/web/screens/general/respsidemenu/ExtMainScreen.java
+++ modules/web/src/com/company/erp/web/screens/general/respsidemenu/ExtMainScreen.java
@@ -1,13 +1,29 @@
 package com.company.erp.web.screens.general.respsidemenu;
 
-import com.haulmont.cuba.gui.screen.LoadDataBeforeShow;
-import com.haulmont.cuba.gui.screen.UiController;
-import com.haulmont.cuba.gui.screen.UiDescriptor;
+import com.company.erp.web.screens.general.dashboards.DashboardScreen;
+import com.haulmont.addon.dashboard.model.Dashboard;
+import com.haulmont.cuba.gui.ScreenBuilders;
+
+import com.haulmont.cuba.gui.Screens;
+import com.haulmont.cuba.gui.components.mainwindow.AppWorkArea;
+import com.haulmont.cuba.gui.components.mainwindow.SideMenu;
+
+import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.cuba.web.app.main.MainScreen;
+import com.haulmont.cuba.web.gui.components.mainwindow.WebAppWorkArea;
+
+import javax.inject.Inject;
+
+
+import java.util.function.Consumer;
+
+import static com.haulmont.cuba.gui.components.mainwindow.AppWorkArea.State.INITIAL_LAYOUT;
 
 
 @UiController("extMainScreen")
 @UiDescriptor("ext-main-screen.xml")
 @LoadDataBeforeShow
 public class ExtMainScreen extends MainScreen {
+
+
 }
