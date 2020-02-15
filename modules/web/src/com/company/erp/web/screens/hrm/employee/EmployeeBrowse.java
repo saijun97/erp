afdae605aed39a5493ed74dfce5ad75fce611d36
SRJ-Acer <saiyaljuttun@hotmail.com>
2020-02-15 16:26:48 +0400

    added attachments tab and attachable functionality

--- modules/web/src/com/company/erp/web/screens/hrm/employee/EmployeeBrowse.java
+++ modules/web/src/com/company/erp/web/screens/hrm/employee/EmployeeBrowse.java
@@ -1,11 +1,37 @@
 package com.company.erp.web.screens.hrm.employee;
 
+import com.haulmont.cuba.gui.WindowManager;
+import com.haulmont.cuba.gui.components.ButtonsPanel;
+import com.haulmont.cuba.gui.components.GroupTable;
+import com.haulmont.cuba.gui.components.ListComponent;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.erp.entity.hrm.employee.Employee;
+import de.diedavids.cuba.attachable.web.WithAttachmentsSupport;
+
+import javax.inject.Inject;
 
 @UiController("erp_Employee.browse")
 @UiDescriptor("employee-browse.xml")
 @LookupComponent("employeesTable")
 @LoadDataBeforeShow
-public class EmployeeBrowse extends StandardLookup<Employee> {
+public class EmployeeBrowse extends StandardLookup<Employee> implements WithAttachmentsSupport {
+    @Inject
+    private GroupTable<Employee> employeesTable;
+    @Inject
+    private ButtonsPanel buttonsPanel;
+
+    @Override
+    public ListComponent getListComponentForAttachments() {
+        return employeesTable;
+    }
+
+    @Override
+    public ButtonsPanel getButtonsPanelForAttachments() {
+        return buttonsPanel;
+    }
+
+    @Override
+    public WindowManager.OpenType attachmentListOpenType() {
+        return WindowManager.OpenType.DIALOG;
+    }
 }
