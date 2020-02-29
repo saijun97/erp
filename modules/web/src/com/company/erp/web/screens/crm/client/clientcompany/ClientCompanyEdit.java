e31ac8d9d0a107a04024ceab28aef70ff4add36e
SRJ-Acer <saiyaljuttun@hotmail.com>
2020-02-29 12:26:22 +0400

    fixed business logic for generating billing and shipping address values and improved code readability

--- modules/web/src/com/company/erp/web/screens/crm/client/clientcompany/ClientCompanyEdit.java
+++ modules/web/src/com/company/erp/web/screens/crm/client/clientcompany/ClientCompanyEdit.java
@@ -2,9 +2,8 @@ package com.company.erp.web.screens.crm.client.clientcompany;
 
 import com.company.erp.entity.general.country.City;
 import com.company.erp.entity.general.country.Country;
-import com.haulmont.cuba.gui.components.LookupPickerField;
-import com.haulmont.cuba.gui.components.TextArea;
-import com.haulmont.cuba.gui.components.TextField;
+import com.haulmont.cuba.gui.Dialogs;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.erp.entity.crm.client.subclasses.company.ClientCompany;
 
@@ -28,57 +27,114 @@ public class ClientCompanyEdit extends StandardEditor<ClientCompany> {
     protected TextField<String> companyAddressField;
     @Inject
     protected TextArea<String> billingAddressField;
+    @Inject
+    private ResizableTextArea<String> shippingAddressField;
+    @Inject
+    protected Dialogs dialogs;
 
-    String displayNameValue;
-    String billingAddressValue;
+    String prevCompanyAddressFieldValue;
+    City prevCityFieldValue;
 
     @Subscribe
-    protected void onBeforeCommitChanges(BeforeCommitChangesEvent event) {
+    public void onAfterShow(AfterShowEvent event) {
+
+        prevCityFieldValue = cityField.getValue();
+        prevCompanyAddressFieldValue =companyAddressField.getValue();
+
+    }
+
+    @Subscribe("windowCommitBtn")
+    public void onWindowCommitBtnClick(Button.ClickEvent event) {
+
+        String displayNameValue;
+        String currentCompanyAddressValue = getEditedEntity().getCompanyAddress();
+        City currentCityValue = getEditedEntity().getCity();
 
         //setting value of displayName
         displayNameValue = companyNameField.getValue();
 
         displayNameField.setValue(displayNameValue);
 
-        //setting value of billingAddress
-        if(billingAddressField.getValue() == null) {
 
-            billingAddressValue = companyNameField.getValue() + ","  + "\n";
+        if (!currentCityValue.equals(prevCityFieldValue) | !currentCompanyAddressValue.equals(prevCompanyAddressFieldValue) | getEditedEntity().getBillingAddress() == null) {
 
-            if (companyAddressField.getValue() != null) {
-
-                billingAddressValue = companyAddressField.getValue() + "," + "\n";
-
-                if (cityField.getValue() != null) {
-
-                    billingAddressValue += cityField.getValue().getCity() + "," + "\n";
-                }
-
-                if(countryField.getValue() != null) {
-
-                    billingAddressValue +=  countryField.getValue().getCountry() + "," + "\n";
-                }
-
-            }
-
-            if (companyAddressField.getValue() == null) {
-
-                if (cityField.getValue() != null) {
-
-                    billingAddressValue = cityField.getValue().getCity() + "," + "\n";
-                }
-
-                if(countryField.getValue() != null) {
-
-                    billingAddressValue +=  countryField.getValue().getCountry() + "," + "\n";
-                }
-
-            }
-
-            billingAddressField.setValue(billingAddressValue);
+            showConfirmationDialogForGeneratingBillingAndShippingAddress();
+            prevCityFieldValue = currentCityValue;
+            prevCompanyAddressFieldValue = currentCompanyAddressValue;
 
         }
 
     }
+
+    @Subscribe("countryField")
+    public void onCountryFieldValueChange(HasValue.ValueChangeEvent<Country> event) {
+
+        cityField.clear();
+
+    }
+
+    public void showConfirmationDialogForGeneratingBillingAndShippingAddress() {
+
+        dialogs.createOptionDialog()
+                .withCaption("Update Billing and Shipping Address Confirmation")
+                .withMessage("Address fields of this client has changed." +
+                        " Would you like to update Bill/Invoice To and Shipping" +
+                        " address fields? Please note any custom text added to current" +
+                        " value of those fields will be overwritten.")
+                .withType(Dialogs.MessageType.CONFIRMATION)
+                .withActions(
+                        new DialogAction(DialogAction.Type.YES) {
+                            @Override
+                            public void actionPerform(Component component) {
+
+                                billingAddressField.setValue(generateBillingAndShippingAddressValue());
+                                shippingAddressField.setValue(generateBillingAndShippingAddressValue());
+                            }
+                        },
+                        new DialogAction(DialogAction.Type.NO)
+                )
+                .show();
+
+    }
+
+     public String generateBillingAndShippingAddressValue() {
+
+         String AddressValue;
+
+         //computing value of billingAddress and shippingAddress
+         AddressValue = companyNameField.getValue() + ","  + "\n";
+
+         if (companyAddressField.getValue() != null) {
+
+             AddressValue = companyAddressField.getValue() + "," + "\n";
+
+             if (cityField.getValue() != null) {
+
+                 AddressValue += cityField.getValue().getCity() + "," + "\n";
+             }
+
+             if(countryField.getValue() != null) {
+
+                 AddressValue +=  countryField.getValue().getCountry() + "," + "\n";
+             }
+
+         }
+
+         if (companyAddressField.getValue() == null) {
+
+             if (cityField.getValue() != null) {
+
+                 AddressValue = cityField.getValue().getCity() + "," + "\n";
+             }
+
+             if(countryField.getValue() != null) {
+
+                 AddressValue +=  countryField.getValue().getCountry() + "," + "\n";
+             }
+
+         }
+
+         return AddressValue;
+     }
 
 }
