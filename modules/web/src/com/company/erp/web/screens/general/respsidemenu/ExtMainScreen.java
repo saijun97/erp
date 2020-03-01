926e5a98900c3317e74f86308d8e7e825812a068
SRJ-Acer <saiyaljuttun@hotmail.com>
2020-03-01 11:47:30 +0400

    added quick links component

--- modules/web/src/com/company/erp/web/screens/general/respsidemenu/ExtMainScreen.java
+++ modules/web/src/com/company/erp/web/screens/general/respsidemenu/ExtMainScreen.java
@@ -1,29 +1,103 @@
 package com.company.erp.web.screens.general.respsidemenu;
 
-import com.company.erp.web.screens.general.dashboards.DashboardScreen;
-import com.haulmont.addon.dashboard.model.Dashboard;
+import com.company.erp.entity.crm.client.superclasses.Client;
+import com.company.erp.entity.crm.srf.ServiceRequest;
+import com.company.erp.entity.general.contact_person.ContactPerson;
+import com.company.erp.entity.general.task.Task;
+import com.company.erp.entity.hrm.employee.Employee;
+import com.company.erp.entity.sales_inventory.order.Order;
+import com.company.erp.entity.sales_inventory.order.joined.payment.Payment;
+import com.company.erp.entity.sales_inventory.product.Product;
 import com.haulmont.cuba.gui.ScreenBuilders;
-
-import com.haulmont.cuba.gui.Screens;
-import com.haulmont.cuba.gui.components.mainwindow.AppWorkArea;
-import com.haulmont.cuba.gui.components.mainwindow.SideMenu;
-
+import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.cuba.web.app.main.MainScreen;
-import com.haulmont.cuba.web.gui.components.mainwindow.WebAppWorkArea;
 
 import javax.inject.Inject;
-
-
-import java.util.function.Consumer;
-
-import static com.haulmont.cuba.gui.components.mainwindow.AppWorkArea.State.INITIAL_LAYOUT;
-
 
 @UiController("extMainScreen")
 @UiDescriptor("ext-main-screen.xml")
 @LoadDataBeforeShow
 public class ExtMainScreen extends MainScreen {
+
+    @Inject
+    private ScreenBuilders screenBuilders;
+
+    @Subscribe("createTaskBtn")
+    public void onCreateTaskBtnClick(Button.ClickEvent event) {
+
+        screenBuilders.editor(Task.class, this)
+                .newEntity()
+                .build()
+                .show();
+
+    }
+
+    @Subscribe("createServiceRequest")
+    public void onCreateServiceRequestClick(Button.ClickEvent event) {
+
+        screenBuilders.editor(ServiceRequest.class, this)
+                .newEntity()
+                .build()
+                .show();
+
+    }
+
+    @Subscribe("createOrder")
+    public void onCreateOrderClick(Button.ClickEvent event) {
+
+        screenBuilders.editor(Order.class, this)
+                .newEntity()
+                .build()
+                .show();
+
+    }
+
+    @Subscribe("createPayment")
+    public void onCreatePaymentClick(Button.ClickEvent event) {
+
+        screenBuilders.editor(Payment.class, this)
+                .newEntity()
+                .build()
+                .show();
+
+    }
+
+    @Subscribe("viewEmployees")
+    public void onViewEmployeesClick(Button.ClickEvent event) {
+
+        screenBuilders.lookup(Employee.class, this)
+                .build()
+                .show();
+
+    }
+
+    @Subscribe("viewContactPersons")
+    public void onViewContactPersonsClick(Button.ClickEvent event) {
+
+        screenBuilders.lookup(ContactPerson.class, this)
+                .build()
+                .show();
+
+    }
+
+    @Subscribe("viewClients")
+    public void onViewClientsClick(Button.ClickEvent event) {
+
+        screenBuilders.lookup(Client.class, this)
+                .build()
+                .show();
+
+    }
+
+    @Subscribe("viewProducts")
+    public void onViewProductsClick(Button.ClickEvent event) {
+
+        screenBuilders.lookup(Product.class, this)
+                .build()
+                .show();
+
+    }
 
 
 }
