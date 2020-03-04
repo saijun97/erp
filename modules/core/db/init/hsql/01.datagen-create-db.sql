3613dbc813747959b0d0ddb5c8cb1ccfe1771f84
SRJ-Acer <saiyaljuttun@hotmail.com>
2020-03-04 11:20:48 +0400

    removed datagen plugin

--- modules/core/db/init/hsql/01.datagen-create-db.sql
+++ modules/core/db/init/hsql/01.datagen-create-db.sql
@@ -1,13 +1,1 @@
--- begin DATAGEN_GENERATED_ENTITY
-create table DATAGEN_GENERATED_ENTITY (
-    ID varchar(36) not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    --
-    ENTITY_NAME varchar(255),
-    ENTITY_ID varchar(255),
-    INST_NAME longvarchar,
-    --
-    primary key (ID)
-)^
--- end DATAGEN_GENERATED_ENTITY
+
