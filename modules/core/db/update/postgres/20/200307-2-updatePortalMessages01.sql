16c7be995e57dbb2776df087408fb66c02dd70df
SRJ-Acer <saiyaljuttun@hotmail.com>
2020-03-07 23:16:32 +0400

    optimised database scripts

--- modules/core/db/update/postgres/20/200307-2-updatePortalMessages01.sql
+++
@@ -1,3 +0,0 @@
-alter table ERP_PORTAL_MESSAGES add column RECEIVED_TS timestamp ^
-update ERP_PORTAL_MESSAGES set RECEIVED_TS = current_timestamp where RECEIVED_TS is null ;
-alter table ERP_PORTAL_MESSAGES alter column RECEIVED_TS set not null ;
