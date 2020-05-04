fcec986cd11bd964d3a1341b47d684fd9ea62110
SRJ-Acer <saiyaljuttun@hotmail.com>
2020-05-05 03:10:26 +0400

    refactored job position screen into master-detail screen

--- modules/web/src/com/company/erp/web/screens/hrm/job_position/Job_PositionBrowse.java
+++ modules/web/src/com/company/erp/web/screens/hrm/job_position/Job_PositionBrowse.java
@@ -5,7 +5,7 @@ import com.company.erp.entity.hrm.job_positions.Job_Position;
 
 @UiController("erp_Job_Position.browse")
 @UiDescriptor("job_position-browse.xml")
-@LookupComponent("job_PositionsTable")
+@LookupComponent("table")
 @LoadDataBeforeShow
-public class Job_PositionBrowse extends StandardLookup<Job_Position> {
+public class Job_PositionBrowse extends MasterDetailScreen<Job_Position> {
 }
