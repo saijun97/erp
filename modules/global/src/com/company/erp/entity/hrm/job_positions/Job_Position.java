0ee2d17cfbef6882cb259a63537e9c613d9d1667
SRJ-Acer <saiyaljuttun@hotmail.com>
2020-05-05 03:09:43 +0400

    refactored employee tables into grouptables and enabled multilinecells feature

--- modules/global/src/com/company/erp/entity/hrm/job_positions/Job_Position.java
+++ modules/global/src/com/company/erp/entity/hrm/job_positions/Job_Position.java
@@ -1,13 +1,15 @@
 package com.company.erp.entity.hrm.job_positions;
 
 import com.company.erp.entity.general.superclasses.NamedEntity;
+import com.company.erp.entity.hrm.employee.Employee;
+import com.haulmont.chile.core.annotations.Composition;
 import com.haulmont.chile.core.annotations.NamePattern;
 import com.haulmont.cuba.core.entity.StandardEntity;
+import com.haulmont.cuba.core.entity.annotation.OnDelete;
+import com.haulmont.cuba.core.global.DeletePolicy;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Lob;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @NamePattern("%s|name")
 @Table(name = "ERP_JOB_POSITION")
@@ -18,6 +20,19 @@ public class Job_Position extends NamedEntity {
     @Lob
     @Column(name = "RESPONSIBILITIES")
     protected String responsibilities;
+
+    @Composition
+    @OnDelete(DeletePolicy.CASCADE)
+    @OneToMany(mappedBy = "jobPosition")
+    protected List<Employee> employee;
+
+    public List<Employee> getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(List<Employee> employee) {
+        this.employee = employee;
+    }
 
     public String getResponsibilities() {
         return responsibilities;
